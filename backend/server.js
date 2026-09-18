bb8bb1d19ac284d6b49a40954cfdeea82d51580a
Pranesh-tech850 <praneshwar18@gmail.com>
2026-09-19 00:29:01 +0530

    backend changed

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,13 @@ const pool = new Pool({
         rejectUnauthorized: false
     }
 });
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
+console.log("DB password exists:", !!process.env.DB_PASSWORD);
+console.log("DB password type:", typeof process.env.DB_PASSWORD);
 
 // ========================================
 // TEST DATABASE CONNECTION
